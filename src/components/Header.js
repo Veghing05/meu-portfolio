e7e88a0ee04996b63249538aabcdc7ac7fbb6ea6
Veghing05 <renatosbv27@gmail.com>
2025-03-30 11:15:37 -0300

    feat: mod h1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaCode } from "react-icons/fa";
+
+<h1>Renato Vegh Dev <FaCode /></h1>
+
 
 export default function Header() {
   return (
     <header className="header">
-      <h1>Meu Portfólio</h1>
+      <h1>Renato Vegh Dev <span>&lt;/&gt;</span></h1>
       <nav>
         <Link to="/">Início</Link>
         
