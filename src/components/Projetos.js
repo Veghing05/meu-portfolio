72e324b61f0e10e9c99fd30fa6d8368d0cb1708a
Veghing05 <renatosbv27@gmail.com>
2025-05-01 16:15:40 -0300

    feat: adicionando logo de tecncologias ultilizadas.

--- src/components/Projetos.js
+++ src/components/Projetos.js
@@ -1,16 +1,38 @@
 import React from "react";
 import Jrx from "../assets/Jrx.png"; // Caminho correto dentro de src
 
+// Logos das tecnologias
+import htmlLogo from "../assets/logos/html.png";
+import cssLogo from "../assets/logos/css.png";
+import jsLogo from "../assets/logos/javascript.png";
+import bootstrapLogo from "../assets/logos/bootstrap.png";
+import reactLogo from "../assets/logos/react.png";
+import nodeLogo from "../assets/logos/node.png";
+
+
 export default function Projetos() {
   return (
     <section className="projetos">
       <div className="projeto">
-        <a href="https://jrx-seguros.vercel.app/" target="_blank" rel="noopener noreferrer">
+        <a 
+          href="https://jrx-seguros.vercel.app/" target="_blank" 
+          rel="noopener noreferrer">
           <img src={Jrx} alt="Projeto 1" className="projeto-imagem" />
         </a>
         <h3>Site JRX Seguros</h3>
         <p>Site institucional da corretora JRX Galasse, com layout responsivo e funcionalidades modernas.</p>
+      
+      {/* Tecnologias usadas nesse projeto */}
+      <div className="tecnologias">
+          <img src={htmlLogo} alt="HTML" className="logo-tecnologia" title="HTML" />
+          <img src={cssLogo} alt="CSS" className="logo-tecnologia" title="CSS" />
+          <img src={jsLogo} alt="JavaScript" className="logo-tecnologia" title="JavaScript" />
+          <img src={reactLogo} alt="React" className="logo-tecnologia" title="React" />
+          <img src={nodeLogo} alt="Node.js" className="logo-tecnologia" title="Node.js" />
+          <img src={bootstrapLogo} alt="Bootstrap" className="logo-tecnologia" title="Bootstrap" />
+        </div>
       </div>
+      
 
       <div className="projeto">
         <a href="https://seu-link-projeto-2.com" target="_blank" rel="noopener noreferrer">
--- src/components/ProjetosEbac.js
+++ src/components/ProjetosEbac.js
@@ -1,20 +1,41 @@
 import React from "react";
-import EbacShoes from "../assets/EbacShoes.png"; // Caminho correto dentro de src
+import EbacShoes from "../assets/EbacShoes.png";
 
+// Logos das tecnologias
+import htmlLogo from "../assets/logos/html.png";
+import cssLogo from "../assets/logos/css.png";
+import jsLogo from "../assets/logos/javascript.png";
+import bootstrapLogo from "../assets/logos/bootstrap.png";
 
 export default function ProjetosEBAC() {
   return (
-    <section className="projetos">
+    <section className="projetos">  
       <div className="projeto">
-        <a href="https://ebac-shoes-ten-gamma.vercel.app/" target="_blank" rel="noopener noreferrer">
+        <a 
+          href="https://ebac-shoes-ten-gamma.vercel.app/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
           <img src={EbacShoes} alt="Projeto EBAC 1" className="projeto-imagem" />
         </a>
-        <h3>Projeto EBAC 1</h3>
-        <p>Descrição do projeto EBAC 1.</p>
+        <h3>EbacShoes</h3>
+        <p>Criação do site EbacShoes, loja de calçados.</p>
+
+        {/* Tecnologias usadas nesse projeto */}
+        <div className="tecnologias">
+          <img src={htmlLogo} alt="HTML" className="logo-tecnologia" title="HTML" />
+          <img src={cssLogo} alt="CSS" className="logo-tecnologia" title="CSS" />
+          <img src={jsLogo} alt="JavaScript" className="logo-tecnologia" title="JavaScript" />
+          <img src={bootstrapLogo} alt="Bootstrap" className="logo-tecnologia" title="Bootstrap" />
+        </div>
       </div>
 
       <div className="projeto">
-        <a href="https://projeto-bootstrap-blue-six.vercel.app/" target="_blank" rel="noopener noreferrer">
+        <a 
+          href="https://projeto-bootstrap-blue-six.vercel.app/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
           <img src="/images/projeto2.jpg" alt="Projeto EBAC 2" className="projeto-imagem" />
         </a>
         <h3>Projeto EBAC 2</h3>
@@ -22,12 +43,17 @@ export default function ProjetosEBAC() {
       </div>
 
       <div className="projeto">
-        <a href="https://seu-link-projeto-3.com" target="_blank" rel="noopener noreferrer">
+        <a 
+          href="https://seu-link-projeto-3.com" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
           <img src="/images/projeto3.jpg" alt="Projeto EBAC 3" className="projeto-imagem" />
         </a>
         <h3>Projeto EBAC 3</h3>
         <p>Descrição do projeto EBAC 3.</p>
       </div>
+
     </section>
   );
 }
